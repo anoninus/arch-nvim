d14be4dc4aa865cc44a80684b4241f3e437ff828
pudep <261843105+pudep@users.noreply.github.com>
2026-07-28 15:40:54 +0530

    removed faulty mapps form blink cmp and using default present instead.

--- lua/plugin/completion.lua
+++ lua/plugin/completion.lua
@@ -13,19 +13,6 @@ return {
     opts = {
       keymap = {
         preset = "default",
-        ["<CR>"] = { "select_and_accept", "fallback" },
-        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<C-e>"] = { "hide", "fallback" },
-        ["<C-y>"] = { "select_and_accept", "fallback" },
-        ["<cr>"] = { "select_and_accept", "fallback" },
-
-        ["<Up>"] = { "select_prev", "fallback" },
-        ["<Down>"] = { "select_next", "fallback" },
-        ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
-        ["<C-n>"] = { "select_next", "fallback_to_mappings" },
-
-        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
       },
 
       appearance = {
