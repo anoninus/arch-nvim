3405e5f762c4aaf79c69fc11661694139382684e
pudep <261843105+pudep@users.noreply.github.com>
2026-08-08 20:47:12 +0530

    feat: map other gr+ motions with fzf.lua

--- lua/map/lsp.lua
+++ lua/map/lsp.lua
@@ -6,3 +6,30 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end, { buffer = bufnr, desc = "LSP References (fzf-lua)" })
   end,
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local bufnr = args.buf
+    local fzf = require("fzf-lua")
+    local opts = { async_or_timeout = true }
+
+    vim.keymap.set("n", "grr", function()
+      fzf.lsp_references(opts)
+    end, { buffer = bufnr, desc = "LSP References (fzf-lua)" })
+
+    vim.keymap.set("n", "gri", function()
+      fzf.lsp_implementations(opts)
+    end, { buffer = bufnr, desc = "LSP Implementations (fzf-lua)" })
+
+    vim.keymap.set("n", "grt", function()
+      fzf.lsp_typedefs(opts)
+    end, { buffer = bufnr, desc = "LSP Type Definitions (fzf-lua)" })
+
+    vim.keymap.set("n", "gra", function()
+      fzf.lsp_code_actions(opts)
+    end, { buffer = bufnr, desc = "LSP Code Actions (fzf-lua)" })
+
+
+    vim.keymap.set("n", "grn", vim.lsp.buf.rename, { buffer = bufnr, desc = "LSP Rename" })
+  end,
+})
