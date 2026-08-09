cdb6b1083bd176c0d3cdce7015ba59f7ea01f548
pudep <261843105+pudep@users.noreply.github.com>
2026-08-09 12:11:03 +0530

    refactor: delete unneeded diagPanel/

--- lua/module/init.lua
+++ lua/module/init.lua
@@ -2,10 +2,6 @@ require("module.session.config")
 require("module.ui.statusline")
 require("module.autosave.config")
 require("module.terminal.config")
-require("module.diagPanel.Panel").setup({
-  mode = "hsplit",
-  height = 8,
-})
 require("module.bufferline.config").setup()
 -- not needed to use run because F1 and F12 works fine
 -- and are precise
