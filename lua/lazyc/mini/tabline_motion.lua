3ec23419bedbfdeaeff5743d959e56e897ef0907
pudep <261843105+pudep@users.noreply.github.com>
2026-08-09 20:14:31 +0530

    refactor: remove `<C-c>` based `tabonly!`

--- lua/lazyc/mini/tabline_motion.lua
+++ lua/lazyc/mini/tabline_motion.lua
@@ -1,4 +1,2 @@
 vim.keymap.set({ "n", "t" }, "<C-k>", "<cmd>tabprevious<cr>", { silent = true, desc = "Previous tab" })
 vim.keymap.set({ "n", "t" }, "<C-j>", "<cmd>tabnext<cr>", { silent = true, desc = "Next tab" })
-
-vim.keymap.set({"n", "v", "i"}, "<C-c>", "<cmd>tabonly!<cr>", {silent = true, desc = "Close zombie tabs"})
