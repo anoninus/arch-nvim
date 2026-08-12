e1c06114b100f24788ac8a6d3f8d424385448e7e
pudep <261843105+pudep@users.noreply.github.com>
2026-08-12 19:43:26 +0530

    refactor: remove oil.nvim

--- lua/lazyc/explore/oil.lua
+++ lua/lazyc/explore/oil.lua
@@ -1,15 +1,5 @@
 -- lua/config/oil.lua
 local function setup_oil()
-  require("oil").setup({
-    lsp_file_methods = {
-      enabled = false,
-    },
-    default_file_explorer = true,
-    skip_confirm_for_simple_edits = true,
-    view_options = {
-      show_hidden = true,
-    },
-  })
 end
 
 vim.api.nvim_create_autocmd("VimEnter", {
@@ -18,5 +8,3 @@ vim.api.nvim_create_autocmd("VimEnter", {
     vim.defer_fn(setup_oil, 50)
   end,
 })
-
-vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
