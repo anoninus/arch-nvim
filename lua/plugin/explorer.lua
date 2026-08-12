48c38345dcdd3638df5eed758f78be8c9a605488
pudep <261843105+pudep@users.noreply.github.com>
2026-08-12 19:44:46 +0530

    fix: move `gg` to `gl`.

--- lua/plugin/explorer.lua
+++ lua/plugin/explorer.lua
@@ -83,7 +83,7 @@ return {
       "nvim-lua/plenary.nvim",
     },
     keys = {
-      { "gg", "<cmd>LazyGitCurrentFile<cr>", desc = "Open Repo of current file" },
+      { "gl", "<cmd>LazyGitCurrentFile<cr>", desc = "Open Repo of current file" },
     },
     config = function()
       vim.g.lazygit_floating_window_winblend = 0
