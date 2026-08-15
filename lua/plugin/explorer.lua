33533c4be38225011dbc8f5ac560f57ae5295e34
pudep <261843105+pudep@users.noreply.github.com>
2026-08-15 17:50:46 +0530

    fix(conflict): lazygit triggers `<M-g>`.

--- lua/plugin/explorer.lua
+++ lua/plugin/explorer.lua
@@ -83,7 +83,7 @@ return {
       "nvim-lua/plenary.nvim",
     },
     keys = {
-      { "<C-g>", "<cmd>LazyGitCurrentFile<cr>", desc = "Open Repo of current file" },
+      { "<M-g>", "<cmd>LazyGitCurrentFile<cr>", desc = "Open Repo of current file" },
     },
     config = function()
       vim.g.lazygit_floating_window_winblend = 0
