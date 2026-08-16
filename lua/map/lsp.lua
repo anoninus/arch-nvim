8b620d328b158b5c34dbe06795de82070caeeb83
pudep <261843105+pudep@users.noreply.github.com>
2026-08-16 15:48:02 +0530

    refactor: remove dupelicate grr mapping.

--- lua/map/lsp.lua
+++ lua/map/lsp.lua
@@ -1,15 +1,6 @@
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local bufnr = args.buf
-    vim.keymap.set("n", "grr", function()
-      require("fzf-lua").lsp_references({ async_or_timeout = true })
-    end, { buffer = bufnr, desc = "LSP References (fzf-lua)" })
-  end,
-})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local bufnr = args.buf
     local fzf = require("fzf-lua")
     local opts = { async_or_timeout = true }
 
